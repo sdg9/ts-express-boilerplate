aae624b99d3ab755a5ed7ee56f6b3af2a65aac4b
Steven Goff <steveng9@gmail.com>
2020-09-07 10:20:12 -0500

    Add prettier
    npm i -D prettier eslint-config-prettier eslint-plugin-prettier
    Update .eslintrc.js from ecmaVersion 12 to 2020
    Add .prettierrc.js

---
+++ .prettierrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  semi: true,
+  trailingComma: 'all',
+  singleQuote: true,
+  printWidth: 120,
+  //   singleQuote: true,
+  //   trailingComma: 'all',
+  //   overrides: [
+  //     {
+  //       files: '*.ts',
+  //       options: {
+  //         parser: 'typescript',
+  //       },
+  //     },
+  //   ],
+};
