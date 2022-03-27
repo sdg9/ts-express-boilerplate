a2011cecacb829f80ba5f0c090a80785d6435369
Steven Goff <steveng9@gmail.com>
2022-03-26 21:15:40 -0500

    Update TS boilerplate

--- src/app.ts
+++ src/app.ts
@@ -2,29 +2,8 @@ import * as express from 'express';
 import * as bodyParser from 'body-parser';
 import * as errorhandler from 'strong-error-handler';
 import { healthCheck } from './routes/healthCheck';
-import * as helmet from 'helmet';
-import * as jwt from 'jsonwebtoken';
-import { ACCESS_TOKEN_SECRET } from './utils/environmentVariables';
 
 export const app = express();
-
-function authenticateToken(request: express.Request, response: express.Response, next: express.NextFunction) {
-  const authHeader = request.headers.authorization;
-  const token = authHeader?.split(' ')[1];
-  if (token == null) {
-    return response.sendStatus(401);
-  }
-  return jwt.verify(token, ACCESS_TOKEN_SECRET, (err, payload) => {
-    if (err) {
-      return response.sendStatus(403);
-    }
-    return next();
-  });
-}
-
-app.use(helmet());
-
-app.use(authenticateToken);
 
 // middleware for parsing application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
